73b8e7d90cd9408ac78933acd3c9ad98625eb08c
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2017-12-06 13:49:50 -0500

    Updated from Brython Server: 12/6/2017 1:49:50 PM

--- longestDictionaryWord.py
+++ longestDictionaryWord.py
@@ -1,1 +1,13 @@
+#Diego Aspinwall
+#12-6-17
+#longestDictionaryWord.py
 
+dictionary = open('engmix.txt')
+high = a
+for word in dictionary:
+    l = 0
+    for w in word:
+        if l < len(w):
+            l = len(w)
+            high = w
+print(high)
