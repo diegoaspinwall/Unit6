8901f5afd16fd7e22b2106248d8d620b72b5b6af
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2017-12-06 14:13:37 -0500

    Updated from Brython Server: 12/6/2017 2:13:37 PM

--- howManyWords.py
+++ howManyWords.py
@@ -3,14 +3,10 @@
 #howManyWords.py
 
 dictionary = open('engmix.txt')
-'''
-for i in range(1,23):
+
+for i in range(2,23):
     num = 0
     for word in dictionary:
         if len(word) == i:
             num+=1
     print('There are', num, i, 'letter words')
-'''
-for word in dictionary:
-    if len(word) == 1:
-        print(word)
