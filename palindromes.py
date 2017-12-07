b61efc1fde3070f3c2e712be91ecaaa3241b9dc6
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2017-12-07 14:27:52 -0500

    Updated from Brython Server: 12/7/2017 2:27:52 PM

--- palindromes.py
+++ palindromes.py
@@ -4,6 +4,11 @@
 
 dictionary = open('engmix.txt')
 
+diclist = []
+
+for word in dictionary:
+    diclist.append(word.strip())
+
 for word in dictionary:
     if word == word.reverse():
         print(word)
