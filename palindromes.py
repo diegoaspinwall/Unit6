24361804098c6c3f0658902daaae5f356cb4590c
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2017-12-07 14:39:44 -0500

    Updated from Brython Server: 12/7/2017 2:39:43 PM

--- palindromes.py
+++ palindromes.py
@@ -7,7 +7,7 @@ dictionary = open('engmix.txt')
 for word in dictionary:
     total = 0
     word = word.strip()
-    for i in len(word):
+    for i in range(0,len(word)):
         if word[i] == word[i*(-1)-1]:
             total += 1
     if total == len(word):
