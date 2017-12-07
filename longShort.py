36dd44073a72af37d304a78c9f292876b6a628c0
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2017-12-07 14:54:27 -0500

    Updated from Brython Server: 12/7/2017 2:54:27 PM

--- longShort.py
+++ longShort.py
@@ -4,10 +4,13 @@
 
 alphabet = 'abcdefghijklmnopqrstuvwxyz'
 
+longest = ['']*26
+shortest = ['']*26
+
 dictionary = open('engmix.txt')
-'''
+
 for word in dictionary:
     alphabet.index(word[0])
-'''
+    
 
 print(alphabet.index('b'))
