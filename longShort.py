912c109e5f482f6044b37041911e53d8e9bfc6ab
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2017-12-11 14:26:03 -0500

    Updated from Brython Server: 12/11/2017 2:26:03 PM

--- longShort.py
+++ longShort.py
@@ -10,7 +10,7 @@ shortest = ['']*26
 dictionary = open('engmix.txt')
 
 for word in dictionary:
+    if len(word.strip()) < len(longest[alphabet.index[word[0]])
     alphabet.index(word[0])
-    
 
 print(alphabet.index('b'))
