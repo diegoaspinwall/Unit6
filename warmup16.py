74157f3a8cf8e3f7d1046911ff90cebc3fe48623
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2017-12-11 14:19:25 -0500

    Updated from Brython Server: 12/11/2017 2:19:25 PM

--- warmup16.py
+++ warmup16.py
@@ -1,1 +1,14 @@
+#Diego Aspinwall
+#12-11-17
+#warmup16.py
 
+dictionary = open('engmix.txt')
+
+ans = []
+
+for word in dictionary:
+    if word[0] == 'd' and word[-1] == 'a':
+        ans.append(word.strip())
+
+for wrd in ans:
+    print(wrd)
