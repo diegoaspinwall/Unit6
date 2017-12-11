e150e9c05b58809a43576ed99576ac3a5461dd09
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2017-12-11 14:28:18 -0500

    Updated from Brython Server: 12/11/2017 2:28:18 PM

--- longShort.py
+++ longShort.py
@@ -10,9 +10,9 @@ shortest = ['']*26
 dictionary = open('engmix.txt')
 
 for word in dictionary:
-    if len(word.strip()) > len(longest[alphabet.index[word[0]]]):
+    if len(word.strip()) > len(longest[alphabet.index[word.strip()[0]]]):
         longest.append(word.strip)
-    if len(word.strip()) < len(shortest[alphabet.index[word[0]]]):
+    if len(word.strip()) < len(shortest[alphabet.index[word.strip()[0]]]):
         shortest.append(word.strip)
 
 
