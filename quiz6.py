dcf1557bf6e90489a6e2ebb116623d8fe36c1aac
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2017-12-13 13:27:11 -0500

    Updated from Brython Server: 12/13/2017 1:27:10 PM

--- quiz6.py
+++ quiz6.py
@@ -3,6 +3,7 @@
 #quiz6.py
 
 #Program 1
+'''
 dictionary = open('engmix.txt')
 
 wordz = []
@@ -17,5 +18,19 @@ for word in dictionary:
         p+=1
     if c==3 and p==2:
         wordz.append(word)
-
+        
 print(wordz)
+'''
+
+#Program 2
+
+dictionary = open('engmix.txt')
+wordCount = 0
+for word in dictionary:
+    word = word.strip()
+    if word[0]:
+        print(word.strip())
+    wordCount += 1
+
+print('There are',wordCount, 'words in the dictionary')
+
