d5e593245dbe60533846af444db8ef42eaa82f15
diegoaspinwall <31485838+diegoaspinwall@users.noreply.github.com>
2017-12-13 13:24:18 -0500

    Updated from Brython Server: 12/13/2017 1:24:18 PM

--- quiz6.py
+++ quiz6.py
@@ -1,1 +1,20 @@
+#Diego Aspinwall
+#12-13-17
+#quiz6.py
 
+#Program 1
+dictionary = open('engmix.txt')
+
+wordz = []
+
+for word in dictionary:
+    c=0
+    p=0
+    if 'c' in word:
+        c+=1
+    if 'p' in word:
+        p+=1
+    if c==3 and p==2:
+        wordz.append(word.strip())
+
+print(wordz)
